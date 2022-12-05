5db6c1a5fc2aa9d858167be9a3e87c211e61129f
Batur Kacamak <batisonline@gmail.com>
2022-12-05 21:03:15 +0100

    refactor code from procedural to better organized way

--- upwork-filter/upwork-filter.user.js
+++ upwork-filter/upwork-filter.user.js
@@ -21,37 +21,64 @@
 // ==/UserScript==
 
 // eslint-disable-next-line func-names
-(function () {
+class JobsFilter {
+  // A list of countries to be removed from job listings
+  static COUNTRIES = ['India', 'Bangladesh', 'Pakistan', 'Arab'];
 
-  const COUNTRIES = ['India', 'Bangladesh', 'Pakistan', 'Arab'];
-
-  const waitFor = (...selectors) => new Promise((resolve) => {
+  // Waits for the specified elements to exist in the DOM,
+  // and then resolves the returned Promise with the elements.
+  static waitFor = (...selectors) => new Promise((resolve) => {
+    // The time to wait before checking for the elements again (in milliseconds)
     const delay = 5000;
+    // A recursive function that checks for the elements and
+    // resolves the Promise if all elements exist
     const f = () => {
+      // Use the 'map()' method to get an array of the elements
+      // that match the given selectors
       const elements = selectors.map((selector) => document.querySelector(selector));
+      // Check if all elements exist
       if (elements.every((element) => element != null)) {
+        // Resolve the Promise with the elements
         resolve(elements);
       } else {
+        // Set a timeout to call the function again after the specified delay
         setTimeout(f, delay);
       }
     };
+    // Start the recursive loop
     f();
   });
 
-  const removeCountries = () => {
+  // Removes job listings for the countries in the 'COUNTRIES' list
+  static removeCountries() {
+    // Get all elements that have a 'data-test' attribute with the value "client-country"
+    // and all elements that have a 'client-location' class
     document.querySelectorAll('*[data-test="client-country"], .job-tile .client-location').forEach((el, index) => {
-      const country = el.innerHTML;
+      // Get the inner HTML of the element (i.e. the country name)
+      const country = el.textContent;
+      // Check if the country exists, is not empty,
+      // and is included in the 'COUNTRIES' list
       if (
           country
           && country !== ''
-          && COUNTRIES.filter(bannedCountry => country.includes(bannedCountry)).length > 0
+          && JobsFilter.COUNTRIES.filter(bannedCountry => country.includes(bannedCountry)).length > 0
       ) {
+        // Remove the parent element of the element (i.e. the job listing)
         el.closest('.up-card-section, .job-tile').parentNode.removeChild(el.closest('.up-card-section, .job-tile'));
       }
     });
-  };
+  }
 
-  setInterval(removeCountries, 5000);
+  // Initializes the filter by starting an interval that calls the 'removeCountries()' method
+  // and waiting for the specified elements to exist in the DOM before calling the method
+  static init() {
+    // Start an interval that calls the 'removeCountries()' method every 5 seconds
+    setInterval(JobsFilter.removeCountries, 5000);
+    // Wait for the specified elements to exist in the DOM,
+    // and then call the 'removeCountries()' method
+    JobsFilter.waitFor(['.up-card-section', '*[data-test="client-country"]']).then(JobsFilter.removeCountries);
+  }
+}
 
-  waitFor(['.up-card-section', '*[data-test="client-country"]']).then(removeCountries);
-}());
+// Initialize the filter when the script is run
+JobsFilter.init();
