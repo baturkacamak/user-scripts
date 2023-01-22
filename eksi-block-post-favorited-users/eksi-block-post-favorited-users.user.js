2572f09b4c4c6019c7b62176b2a610dc87a27254
baturkacamak <batisonline@gmail.com>
2023-01-22 10:18:18 +0100

    Add JSDocs documentation to EksiDOM, EksiNotification, EksiCSS, and EksiError classes

--- eksi-block-post-favorited-users/eksi-block-post-favorited-users.user.js
+++ eksi-block-post-favorited-users/eksi-block-post-favorited-users.user.js
@@ -16,7 +16,7 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-(function() {
+(function(url, data) {
     'use strict';
 
     const Endpoints = {
@@ -24,6 +24,13 @@
         FAVORITES: 'https://eksisozluk.com/entry/favorileyenler'
     }
 
+    /**
+     * @class EksiError
+     * @extends Error
+     * @desc A custom error class for handling errors in the Eksi Sözlük script.
+     * @param {string} message - The error message.
+     * @param {number} statusCode - The HTTP status code associated with the error.
+     */
     class EksiError extends Error {
         constructor(message, statusCode) {
             super(message);
@@ -32,10 +39,10 @@
     }
 
     /**
-     * @class RemoteRequest
+     * @class EksiRemoteRequest
      * @desc A class for making remote requests.
      */
-    class RemoteRequest {
+    class EksiRemoteRequest {
         /**
          * @function setupXHR
          * @desc Sets up the XHR object with the necessary properties
@@ -112,8 +119,118 @@
          * @param {Object} data - The data to send in the body of the POST request.
          * @returns {Promise<string>} - A promise that resolves with the response of the POST request as a string.
          */
-        async post(url, data) {
+        async post(url, data = {}) {
             return this.makeRequest('POST', url, data);
+        }
+    }
+
+    /**
+     * @class EksiDOM
+     * @desc A class for providing a simplified and consistent interface for manipulating the DOM
+     */
+    class EksiDOM {
+        /**
+         * @function querySelector
+         * @desc Wrapper function for `document.querySelector`
+         * @param {string} selector - The CSS selector to query
+         * @param {HTMLElement} [context=document] - The context element to search within
+         * @returns {HTMLElement} - The first element matching the selector
+         */
+        querySelector(selector, context = document) {
+            return context.querySelector(selector);
+        }
+
+        /**
+         * @function querySelectorAll
+         * @desc Wrapper function for `document.querySelectorAll`
+         * @param {string} selector - The CSS selector to query
+         * @param {HTMLElement} [context=document] - The context element to search within
+         * @returns {NodeList} - A list of elements matching the selector
+         */
+        querySelectorAll(selector, context = document) {
+            return context.querySelectorAll(selector);
+        }
+
+        /**
+         * @function createElement
+         * @desc Wrapper function for `document.createElement`
+         * @param {string} tagName - The name of the element to create
+         * @returns {HTMLElement} - The created element
+         */
+        createElement(tagName) {
+            return document.createElement(tagName);
+        }
+
+        /**
+         * @function addClass
+         * @desc Adds a class to an element
+         * @param {HTMLElement} element - The element to add the class to
+         * @param {string} className - The class to add
+         */
+        addClass(element, className) {
+            element.classList.add(className);
+        }
+
+        /**
+         * @function removeClass
+         * @desc Removes a class from an element
+         * @param {HTMLElement} element - The element to remove the class from
+         * @param {string} className - The class to remove
+         */
+        removeClass(element, className) {
+            element.classList.remove(className);
+        }
+
+        /**
+         * @function hasClass
+         * @desc Check if an element has a class
+         * @param {HTMLElement} element - The element to check
+         * @param {string} className - The class to check for
+         * @returns {boolean} - Whether the element has the class or not
+         */
+        hasClass(element, className) {
+            return element.classList.contains(className);
+        }
+
+        /**
+         * @function toggleClass
+         * @desc Toggles a class on an element
+         * @param {HTMLElement} element - The element to toggle the class on
+         * @param {string} className - The class to toggle
+         */
+        toggleClass(element, className) {
+            element.classList.toggle(className);
+        }
+
+        /**
+         * @function appendChild
+         * @desc Wrapper function for `HTMLElement.appendChild`
+         * @param {HTMLElement} parent - The parent element
+         * @param {HTMLElement} child - The child element to append
+         */
+        appendChild(parent, child) {
+            parent.appendChild(child);
+        }
+
+        /**
+         * @function removeChild
+         * @desc Wrapper function for `HTMLElement.removeChild`
+         * @param {HTMLElement} parent - The parent element
+         * @param {HTMLElement} child - The child element to remove
+         */
+        removeChild(parent, child) {
+            parent.removeChild(child);
+        }
+
+        /**
+         * @function addEventListener
+         * @desc Wrapper function for `HTMLElement.addEventListener`
+         * @param {HTMLElement} element - The element to add the event listener to
+         * @param {string} event - The event to listen for
+         * @param {function} callback - The function to call when the event is triggered
+         */
+        addEventListener(element, event, callback) {
+            element.addEventListener(event, callback);
         }
     }
 
@@ -127,7 +244,7 @@
          * @desc Initializes the class and sets the base URL for the blocking endpoint.
          */
         constructor() {
-            this.remoteRequest = new RemoteRequest();
+            this.remoteRequest = new EksiRemoteRequest();
             this.htmlParser = new EksiHtmlParser();
             this.notification = new EksiNotification();
             this.totalUserCount = 0;
@@ -138,7 +255,7 @@
          * @function fetchFavorites
          * @desc Fetches the HTML response of a request for the list of users who have favorited a specific post.
          * @param {string} entryId - The ID of the entry to fetch the favorited users of.
-         * @returns {Promise<string>} - A promise that resolves with the HTML response of the request for the list of favorited users.
+         * @returns {Promise<Array<string>>} - A promise that resolves with the HTML response of the request for the list of favorited users.
          */
         async fetchFavorites(entryId) {
             try {
@@ -158,10 +275,12 @@
             try {
                 const userUrls = await this.fetchFavorites(entryId);
                 this.totalUserCount = userUrls.length;
-                for (const userUrl of userUrls) {
-                    const userProfileHtml = await this.fetchUserProfile(userUrl);
-                    const userId = this.htmlParser.parseUserIdFromProfile(userProfileHtml);
-                    await this.blockUser(userId);
+                if (userUrls) {
+                    for (const userUrl of userUrls) {
+                        const userProfileHtml = await this.fetchUserProfile(userUrl);
+                        const userId = this.htmlParser.parseUserIdFromProfile(userProfileHtml);
+                        await this.blockUser(userId);
+                    }
                 }
             } catch (error) {
                 throw new EksiError(error.message, error.statusCode);
@@ -191,46 +310,61 @@
             try {
                 const blockTypes = ['i', 'm'];
                 for (const blockType of blockTypes) {
-                    // await this.remoteRequest.post(`${Endpoints.BLOCK}/${userId}?r=${blockType}`);
+                    await this.remoteRequest.post(`${Endpoints.BLOCK}/${userId}?r=${blockType}`);
                 }
-                this.createNotificationPopup(`${this.currentBlocked} blocked out of ${this.totalUserCount}`);
-                this.currentBlocked++;
+                this.updateNotification();
             } catch (error) {
                 throw new EksiError(error.message, error.statusCode);
             }
         }
+
+        /**
+         * @function updateNotification
+         * @desc Updates the notification with the current number of blocked users
+         */
+        updateNotification() {
+            this.notification.show(`${this.currentBlocked} blocked out of ${this.totalUserCount}`);
+            this.currentBlocked++;
+        }
+
     }
 
+    /**
+     * @class EksiNotification
+     * @desc A class for creating and displaying notifications on the page.
+     */
     class EksiNotification {
         /**
          * @constructor
          * @desc Initializes the class
-         * @param {string} message - The message to be displayed in the notification popup
+         */
+        constructor() {
+            this.cssHandler = new EksiCSS();
+            this.domHandler = new EksiDOM();
+        }
+
+        /**
+         * @function show
+         * @desc Displays the notification with the given message
+         * @param {string} message - The message to be displayed in the notification
+         * @param {object} options - Additional options for the notification
+         * @param {string} options.css - The css to be applied to the notification container
+         * @param {number} options.timeout - The timeout for removing the notification element
+         */
+        show(message, options = {}) {
+            this.applyStyles(options.css);
+            this.createElement(message);
+            this.createCloseButton();
+            this.appendNotificationToDOM();
+            this.setAutoCloseTimeout(options.timeout);
+        }
+
+        /**
+         * @function applyStyles
+         * @desc Adds the default styles for the notification container. If custom styles have been passed, they will be added as well
          * @param {string} css - The css to be applied to the notification container
          */
-        constructor(message, css) {
-            this.cssHandler = new EksiCSS();
-            this.message = message;
-            this.css = css;
-        }
-
-        /**
-     * @function show
-     * @desc Displays the notification with the given message
-     * @param {string} message - The message to be displayed in the notification
-     */
-        show(message) {
-            this.addStyles();
-            this.createNotificationElement(message);
-            this.appendToBody();
-            this.setTimeout();
-        }
-
-        /**
-         * @function addStyles
-         * @desc Adds the default styles for the notification container. If custom styles have been passed, they will be added as well
-         */
-        addStyles() {
+        applyStyles(css) {
             let defaultCSS = `.notification-container {
                 position: fixed;
                 top: 20px;
@@ -245,50 +379,68 @@
             }`;
 
             // If custom styles have not been passed, use the default styles
-            if(!this.css) {
-                this.css = defaultCSS;
+            if(!css) {
+                css = defaultCSS;
             }
 
             // Add the styles to the page
-            this.cssHandler.addCSS(this.css);
+            this.cssHandler.addCSS(css);
         }
 
         /**
-     * @function createNotificationElement
-     * @desc Creates the notification element with the given message
-     * @param {string} message - The message to be displayed in the notification
-     */
-        createNotificationElement(message) {
-            this.notificationElement = document.createElement('div');
-            this.notificationElement.classList.add('notification-container');
+         * @function createElement
+         * @desc Creates the notification element with the given message
+         * @param {string} message - The message to be displayed in the notification
+         */
+        createElement(message) {
+            this.notificationElement = this.domHandler.createElement('div');
+            this.domHandler.addClass(this.notificationElement, 'notification-container')
             this.notificationElement.innerHTML = `<p>${message}</p>`;
         }
 
         /**
-     * @function appendToBody
-     * @desc Appends the notification element to the body
-     */
-        appendToBody() {
-            document.body.appendChild(this.notificationElement);
+         * @function createCloseButton
+         * @desc Adds a close button to the notification element
+         */
+        createCloseButton() {
+          let closeButton = this.domHandler.createElement('span');
+          closeButton.innerHTML = 'X';
+          this.domHandler.addClass(closeButton, 'close-button');
+          this.domHandler.addEventListener(closeButton, 'click', () => {
+              this.notificationElement.remove();
+          });
+          this.domHandler.appendChild(this.notificationElement, closeButton);
         }
 
         /**
-     * @function setTimeout
-     * @desc Sets a timeout to remove the notification element after a certain amount of time
-     */
-        setTimeout() {
+         * @function appendNotificationToDOM
+         * @desc Appends the notification element to the body
+         */
+        appendNotificationToDOM() {
+            this.domHandler.appendChild(document.body, this.notificationElement)
+        }
+
+        /**
+         * @function setAutoCloseTimeout
+         * @desc Sets a timeout to remove the notification element after a certain amount of time
+         */
+        setAutoCloseTimeout(timeout = 30000) {
             setTimeout(() => {
                 this.notificationElement.remove();
-            }, 30000);
+            }, timeout);
         }
     }
-
 
     /**
      * @class EksiHtmlParser
      * @desc A class for parsing HTML responses from Eksi Sözlük.
      */
     class EksiHtmlParser {
+
+        constructor() {
+            this.domHandler = new EksiDOM();
+        }
+
         /**
          * @function parseHtml
          * @desc Parses the HTML string and returns a document object.
@@ -307,7 +459,7 @@
          */
         parseFavoritesHtml(html) {
             const ul = this.parseHtml(html);
-            const anchorTags = ul.querySelectorAll('li a');
+            const anchorTags = this.domHandler.querySelectorAll('li a', ul);
             const hrefs = [];
             anchorTags.forEach((a) => {
                 if (a.href.includes('biri')) {
@@ -326,7 +478,7 @@
         parseUserIdFromProfile(html) {
             try {
                 const doc = this.parseHtml(html);
-                const input = doc.querySelector('#who');
+                const input = this.domHandler.querySelector('#who');
                 if(input) {
                     return input.value;
                 } else {
@@ -338,6 +490,10 @@
         }
     }
 
+    /**
+     * @class EksiCSS
+     * @desc A class for adding and manipulating CSS styles on the page
+     */
     class EksiCSS {
         /**
      * @constructor
@@ -345,6 +501,7 @@
      */
         constructor() {
             this.styleTagId = 'eksi-css-style';
+            this.domHandler = new EksiDOM();
         }
 
         /**
@@ -362,10 +519,10 @@
      * @returns {HTMLStyleElement} - The newly created style tag
      */
         createStyleTag() {
-            const style = document.createElement('style');
+            const style = this.domHandler.createElement('style');
             style.id = this.styleTagId;
             style.type = 'text/css';
-            document.head.appendChild(style);
+            this.domHandler.appendChild(document.head, style);
             return style;
         }
 
@@ -392,7 +549,7 @@
             }
             // If the CSS has not been added before, append it to the style tag
             if (!this.hasCSSAdded(css)) {
-                style.appendChild(document.createTextNode(css));
+                this.domHandler.appendChild(style, document.createTextNode(css));
             }
         }
     }
@@ -402,18 +559,22 @@
      * @desc A class for creating and adding a menu item to dropdown for blocking multiple users in bulk on by fetching the list of users who have favorited a specific post
      */
     class EksiBlockUsersUI {
+        constructor() {
+            this.domHandler = new EksiDOM();
+        }
+
         /**
          * @function createMenuItemElements
          * @desc creates a new li and a elements and sets their attributes
          * @returns {HTMLElement} the li element
          */
         createMenuItemElements() {
-            const newItem = document.createElement('li');
-            const newAnchor = document.createElement('a');
+            const newItem = this.domHandler.createElement('li');
+            const newAnchor = this.domHandler.createElement('a');
             newAnchor.setAttribute('title', 'favorileyenleri blokla');
             newAnchor.setAttribute('aria-label', 'favorileyenleri blokla');
             newAnchor.innerText = 'favorileyenleri blokla';
-            newItem.appendChild(newAnchor);
+            this.domHandler(newItem, newAnchor);
             return newItem;
         }
 
@@ -424,7 +585,7 @@
          * @param {HTMLElement} menuItem - the li element
          */
         addMenuItemEventListener(entryId, menuItem) {
-            menuItem.addEventListener('click', () => {
+            this.domHandler.addEventListener(menuItem, 'click', () => {
                 const blockUsers = new EksiBlockUsers();
                 blockUsers.blockUsers(entryId);
             });
@@ -448,7 +609,7 @@
          */
         addMenuItemToDropdown() {
             // Select all the dropdown menus
-            const dropdownMenus = document.querySelectorAll('.feedback-container .other.dropdown ul.dropdown-menu.right.toggles-menu');
+            const dropdownMenus = this.domHandler.querySelectorAll('.feedback-container .other.dropdown ul.dropdown-menu.right.toggles-menu');
             // Iterate over the dropdown menus
             dropdownMenus.forEach(dropdownMenu => {
                 // Find the entry id of the post
@@ -456,7 +617,7 @@
                 // Create a menu item for blocking multiple users
                 const menuItem = this.createMenuItem(entryId);
                 // Append the menu item to the dropdown menu
-                dropdownMenu.appendChild(menuItem);
+                this.domHandler(dropdownMenu, menuItem);
             });
         }
     }
@@ -467,7 +628,6 @@
      */
     (function() {
         const blockUsersUI = new EksiBlockUsersUI();
-        blockUsersUI.addCSS();
         blockUsersUI.addMenuItemToDropdown();
     })();
 })();
