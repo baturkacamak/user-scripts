d268d02b96b8f58e1d94f6f945f1ee3ac31479be
baturkacamak <batisonline@gmail.com>
2023-07-07 19:07:20 +0200

    Add server.js for local development server
    Created a server.js file with an Express server setup to serve files from the current directory. The server runs on port 3000 by default. This allows for local development and testing of userscripts without the need to copy and paste them into Tampermonkey's script UI.
    
    Modified package.json to include Express as a dependency.
    
    Changes:
    - Added server.js
    - Modified package.json
    
    Dependencies:
    - Added express@4.18.2

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
