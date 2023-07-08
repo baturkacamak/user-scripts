3475c7e0d0606f68117582bbc51b6ce8c312a61c
baturkacamak <batisonline@gmail.com>
2023-07-08 17:20:05 +0200

    Update server.js
    - Added a comment to disable linting on the console.log statement
    
    This commit adds a comment `// eslint-disable-next-line no-console` to disable linting for the `console.log` statement in the server.js file.

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const PORT = 3000;
 app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
   console.log(`Server is running at http://localhost:${PORT}`);
 });
