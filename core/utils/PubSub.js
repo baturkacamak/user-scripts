03ab6cefd6cae435d357d0182b17743df58a65be
baturkacamak <batisonline@gmail.com>
2025-04-19 18:01:50 +0200

    feat: integrate PubSub with SelectBox and enhance event broadcasting
    - Enhanced `SelectBox` to emit PubSub events on dropdown open, close, and destroy actions using `selectbox:*` event namespaces.
    - Refactored and expanded `PubSub` utility:
      - Introduced unique `subscriptionId` for each subscriber.
      - Added `unsubscribe(subscriptionId)` for granular removal of listeners.
      - Added `clear(event?)` to remove specific or all subscriptions.
      - Improved encapsulation using private `#events` field.
    - Refactored `SelectBox` structure for better readability and added inline documentation for public methods.
    
    These changes decouple components and enable reactive behavior between UI elements through event-driven architecture.

--- core/utils/PubSub.js
+++ core/utils/PubSub.js
@@ -1,23 +1,67 @@
-import Logger from "./Logger";
-
+/**
+ * PubSub - A simple publish/subscribe pattern implementation
+ * Enables components to communicate without direct references
+ */
 class PubSub {
-    static events = {};
+    static #events = {};
 
-    static subscribe(eventName, callback) {
-        if (!this.events[eventName]) {
-            this.events[eventName] = [];
+    /**
+     * Subscribe to an event
+     * @param {string} event - Event name
+     * @param {Function} callback - Callback function
+     * @return {string} Subscription ID
+     */
+    static subscribe(event, callback) {
+        if (!this.#events[event]) {
+            this.#events[event] = [];
         }
-        this.events[eventName].push(callback);
 
-        // Return unsubscribe function
-        return () => {
-            this.events[eventName] = this.events[eventName].filter(cb => cb !== callback);
-        };
+        const subscriptionId = `${event}_${Date.now()}_${Math.random().toString(36).substring(2, 9)}`;
+        this.#events[event].push({callback, subscriptionId});
+        return subscriptionId;
     }
 
-    static publish(eventName, data) {
-        if (!this.events[eventName]) return;
-        this.events[eventName].forEach(callback => callback(data));
+    /**
+     * Unsubscribe from an event
+     * @param {string} subscriptionId - Subscription ID
+     * @return {boolean} Success state
+     */
+    static unsubscribe(subscriptionId) {
+        for (const event in this.#events) {
+            const index = this.#events[event].findIndex(sub => sub.subscriptionId === subscriptionId);
+            if (index !== -1) {
+                this.#events[event].splice(index, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Publish an event
+     * @param {string} event - Event name
+     * @param {any} data - Data to pass to subscribers
+     */
+    static publish(event, data) {
+        if (!this.#events[event]) {
+            return;
+        }
+
+        this.#events[event].forEach(sub => {
+            sub.callback(data);
+        });
+    }
+
+    /**
+     * Clear all subscriptions
+     * @param {string} [event] - Optional event name to clear only specific event
+     */
+    static clear(event) {
+        if (event) {
+            delete this.#events[event];
+        } else {
+            this.#events = {};
+        }
     }
 }
 
