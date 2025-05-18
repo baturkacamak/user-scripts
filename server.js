5fe4945ef5459e44a168fa9c72103119d00e3cb4
baturkacamak <batisonline@gmail.com>
2025-05-18 16:58:12 +0200

    feat: add CORS support to the server
    - Integrated the `cors` middleware into the Express server to enable Cross-Origin Resource Sharing.
    - Updated `package.json` to include `cors` as a dependency, ensuring compatibility with the latest version.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
